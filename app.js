09453f6075668d5c67d077fb98d3638e74317a73
stephen-kovolski <s.kovolski@gmail.com>
2023-04-13 23:59:35 -0400

    res.send

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
   if (myMotel[i].available == true){
       
     }
-    //res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(__dirname + "/public/index.html");
 
     res.status(200).json({
       message: "successful get request",
