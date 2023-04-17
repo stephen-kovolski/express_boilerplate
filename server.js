e899ec29ce4db241aa2927982902b6c8318f340a
Stephen Kovolski <s.kovolski@gmail.com>
2023-04-17 19:28:58 -0400

    delete ejs

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const expressLayouts = require('express-ejs-layouts')
 const myMotel = require('./my_motel.js')
 const indexRouter = require('./routes/index.js')
 
-app.set('view engine', 'ejs')
 app.set('views', __dirname + '/view')
-app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
